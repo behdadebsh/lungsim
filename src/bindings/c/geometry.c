06375b1720e5d4a3a1466cfda7a130d14860f7d1
Behdad Shaarbaf Ebrahimi <behdad.ebrahimi@gmail.com>
2023-10-29 01:35:20 +1300

    partial occlusions added

--- src/bindings/c/geometry.c
+++ src/bindings/c/geometry.c
@@ -21,6 +21,7 @@ int get_local_node_f_c(const char *ndimension, int *dimension_len, const char *n
 void define_rad_from_geom_c(const char *order_system, int *order_system_len, double *control_param,
                             const char *start_from, int *start_from_len, double *start_rad,
                             const char *group_type, int *group_type_len, const char *group_options, int *group_options_len);
+void occlude_vessel_c(int *VESSEL_NUMBER, double *RATIO);
 void element_connectivity_1d_c(void);
 void evaluate_ordering_c(void);
 void volume_of_mesh_c(double *volume_model, double *volume_tree);
@@ -127,6 +128,13 @@ void define_rad_from_geom(const char *order_system, double control_param, const 
   int group_options_len = (int)strlen(group_options);
   define_rad_from_geom_c(order_system, &order_system_len, &control_param, start_from, &start_from_len, &start_rad,
                          group_type, &group_type_len, group_options, &group_options_len);
+
+}
+
+void occlude_vessel(int VESSEL_NUMBER, double RATIO)
+{
+
+  occlude_vessel_c(&VESSEL_NUMBER, &RATIO);
 
 }
 
