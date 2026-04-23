20f7ac597ce94b7473fbea938f9d4f68fac9befd
Merryn Tawhai <mhow008@bn442979.uoa.auckland.ac.nz>
2026-04-23 15:34:33 +1200

    added bindings for update_species

--- src/bindings/c/parameter_types.f90
+++ src/bindings/c/parameter_types.f90
@@ -106,4 +106,22 @@ contains
 
   end subroutine update_solve_c
 
+!!!######################################################################
+  
+  subroutine update_species_c(param_name, param_name_len) bind(C, name="update_species_c")
+    use iso_c_binding, only: c_ptr
+    use utils_c, only: strncpy
+    use parameter_types, only: update_species
+    use other_consts, only: MAX_STRING_LEN
+    implicit none
+    
+    integer,intent(in) :: param_name_len
+    type(c_ptr), value, intent(in) :: param_name
+    character(len=MAX_STRING_LEN) :: param_name_f
+
+    call strncpy(param_name_f, param_name, param_name_len)
+    call update_species(param_name_f)
+
+  end subroutine update_species_c
+
 end module parameter_types_c
--- src/bindings/c/parameter_types.h
+++ src/bindings/c/parameter_types.h
@@ -9,5 +9,6 @@ SHO_PUBLIC void update_gasexchange(const char *param_name, double param_value);
 SHO_PUBLIC void update_ventilation(const char *param_name, double param_value);
 SHO_PUBLIC void update_cardiac(const char *param_name, double param_value);
 SHO_PUBLIC void update_solve(const char *param_name, double param_value);
+SHO_PUBLIC void update_species(const char *param_name);
 
 #endif /* AETHER_PARAMETER_TYPES_H */
