1df34241000941ab3cc2d68f4113c1f40521c05f
Merryn Tawhai <mhow008@bn442979.uoa.auckland.ac.nz>
2026-04-23 15:33:40 +1200

    removed content_from_po2, removed some variables from the function calls for steadstate_co2 and steadystate_o2

--- src/bindings/c/gas_exchange.c
+++ src/bindings/c/gas_exchange.c
@@ -1,15 +1,15 @@
 #include "gas_exchange.h"
 
-double content_from_po2_c(double *pco2, double *po2);
-
-double steadystate_co2_c(double *p_art_co20, double *p_art_o2, double *p_ven_co20, double *p_ven_o2, double *Vdot_alv);
+double steadystate_co2_c(double *Vdot_alv);
      
-double content_from_po2(double pco2, double po2)
+double steadystate_o2_c(double *Vdot_alv);
+     
+double steadystate_co2(double Vdot_alv)
 {
-  return content_from_po2_c(&pco2, &po2);
+  return steadystate_co2_c(&Vdot_alv);
 }
 
-double steadystate_co2(double p_art_co20, double p_art_o2, double p_ven_co20, double p_ven_o2, double Vdot_alv)
+double steadystate_o2(double Vdot_alv)
 {
-  return steadystate_co2_c(&p_art_co20, &p_art_o2, &p_ven_co20, &p_ven_o2, &Vdot_alv);
+  return steadystate_o2_c(&Vdot_alv);
 }
--- src/bindings/c/gas_exchange.f90
+++ src/bindings/c/gas_exchange.f90
@@ -9,28 +9,31 @@ contains
   
 !!!######################################################################
   
-  function steadystate_co2_c(p_art_co20, p_art_o2, p_ven_co20, p_ven_o2, Vdot_alv) &
+  function steadystate_co2_c(Vdot_alv) &
        result(p_art_co2) bind(C, name="steadystate_co2_c")
     use gas_exchange, only: steadystate_co2
     
-    real(dp) :: p_art_co20, p_art_o2, p_ven_co20, p_ven_o2, Vdot_alv
+    real(dp) :: Vdot_alv
     real(dp) :: p_art_co2
     
-    p_art_co2 = steadystate_co2(p_art_co20, p_art_o2, p_ven_co20, p_ven_o2, Vdot_alv)
+    p_art_co2 = steadystate_co2(Vdot_alv)
     
   end function steadystate_co2_c
   
 !!!######################################################################
-
-  function content_from_po2_c(pco2, po2) result (c_from_po2) bind(C, name="content_from_po2_c")
-    use gas_exchange, only: content_from_po2
-
-    real(dp) :: pco2, po2
-    real(dp) :: c_from_po2
-
-    c_from_po2 = content_from_po2(pco2, po2)
-
-  end function content_from_po2_c
+  
+  function steadystate_o2_c(Vdot_alv) &
+       result(p_art_o2) bind(C, name="steadystate_o2_c")
+    use gas_exchange, only: steadystate_o2
+    
+    real(dp) :: Vdot_alv
+    real(dp) :: p_art_o2
+    
+    p_art_o2 = steadystate_o2(Vdot_alv)
+    
+  end function steadystate_o2_c
+  
+!!!######################################################################
   
 end module gas_exchange_c
 
