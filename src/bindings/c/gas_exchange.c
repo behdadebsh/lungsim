e1592f95fe4a036abb9aa22f563d7f573c8733bb
Merryn Tawhai <mhow008@bn442979.uoa.auckland.ac.nz>
2026-05-25 15:32:04 +1200

    bindings for solve_gasexchange, get_ABG_value

--- src/bindings/c/gas_exchange.c
+++ src/bindings/c/gas_exchange.c
@@ -1,8 +1,14 @@
 #include "gas_exchange.h"
 
+#include "string.h"
+
 double steadystate_co2_c(double *Vdot_alv);
      
 double steadystate_o2_c(double *Vdot_alv);
+
+double get_ABG_value_c(const char *request, int *request_len);
+
+void solve_gasexchange_c(double *t_0, double *t_1, const char *phase, int *phase_len, const char *filename, int *filename_len);
      
 double steadystate_co2(double Vdot_alv)
 {
@@ -12,4 +18,17 @@ double steadystate_co2(double Vdot_alv)
 double steadystate_o2(double Vdot_alv)
 {
   return steadystate_o2_c(&Vdot_alv);
+}
+
+double get_ABG_value(const char *request)
+{
+  int request_len = (int)strlen(request);
+  return get_ABG_value_c(request, &request_len);
+}
+
+void solve_gasexchange(double t_0, double t_1, const char *phase, const char *filename)
+{
+  int phase_len = (int)strlen(phase);
+  int filename_len = (int)strlen(filename);
+  solve_gasexchange_c(&t_0, &t_1, phase, &phase_len, filename, &filename_len);
 }
--- src/bindings/c/gas_exchange.f90
+++ src/bindings/c/gas_exchange.f90
@@ -34,6 +34,44 @@ contains
   end function steadystate_o2_c
   
 !!!######################################################################
+
+  function get_ABG_value_c(request, request_len) result(my_value) &
+       bind(C, name="get_ABG_value_c")
+    use iso_c_binding, only: c_ptr
+    use utils_c, only: strncpy
+    use other_consts, only: MAX_FILENAME_LEN
+    use gas_exchange, only: get_ABG_value
+
+    integer, intent(in) :: request_len
+    type(c_ptr), value, intent(in) :: request
+    character(len=MAX_FILENAME_LEN) :: request_f
+    real(dp) :: my_value
+    
+    call strncpy(request_f, request, request_len)
+    my_value = get_ABG_value(request_f)
+
+  end function get_ABG_value_c
+    
+!!!######################################################################
+
+  subroutine solve_gasexchange_c(t_0, t_1, phase, phase_len, filename, filename_len) &
+       bind(C, name="solve_gasexchange_c")
+    use iso_c_binding, only: c_ptr
+    use utils_c, only: strncpy
+    use other_consts, only: MAX_FILENAME_LEN
+    use gas_exchange, only: solve_gasexchange
+
+    integer, intent(in) :: phase_len, filename_len
+    real(dp), intent(in) :: t_0, t_1
+    type(c_ptr), value, intent(in) :: phase, filename
+    character(len=MAX_FILENAME_LEN) :: phase_f, filename_f
+
+    call strncpy(phase_f, phase, phase_len)
+    call strncpy(filename_f, filename, filename_len)
+
+    call solve_gasexchange(t_0, t_1, phase_f, filename_f)
+
+  end subroutine solve_gasexchange_c
   
 end module gas_exchange_c
 
